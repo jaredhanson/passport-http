11df45fb8cbeecf4efff3178df30c718008eddbe
Jared Hanson <jaredhanson@gmail.com>
2011-10-27 08:29:25 -0700

    Initial implementation of DigestStrategy.

---
+++ test/strategies/digest-test.js
@@ -0,0 +1,75 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var DigestStrategy = require('passport-http/strategies/digest');
+
+
+vows.describe('DigestStrategy').addBatch({
+  
+  'strategy': {
+    topic: function() {
+      return new DigestStrategy(
+        function() {},
+        function() {}
+      );
+    },
+    
+    'should be named digest': function (strategy) {
+      assert.equal(strategy.name, 'digest');
+    },
+  },
+  
+  'strategy handling a valid request': {
+    topic: function() {
+      var strategy = new DigestStrategy(
+        function(username, done) {
+          done(null, 'secret');
+        },
+        function(username, options, done) {
+          done(null, { username: username });
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(null, user);
+        }
+        strategy.fail = function() {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.method = 'HEAD';
+        req.headers = {};
+        req.headers.authorization = 'Digest username="bob", realm="Users", nonce="NOIEDJ3hJtqSKaty8KF8xlkaYbItAkiS", uri="/", response="22e3e0a9bbefeb9d229905230cb9ddc8"';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user) {
+        assert.equal(user.username, 'bob');
+      },
+    },
+  },
+  
+  'strategy constructed without a secret callback or validate callback': {
+    'should throw an error': function (strategy) {
+      assert.throws(function() { new BasicStrategy() });
+    },
+  },
+  
+  'strategy constructed without a validate callback': {
+    'should throw an error': function (strategy) {
+      assert.throws(function() { new BasicStrategy(function() {}) });
+    },
+  },
+  
+}).export(module);
