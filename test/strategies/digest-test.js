882b963fddcdcf9606b48f304d21d32efcaeca41
Jared Hanson <jaredhanson@gmail.com>
2011-10-29 13:23:05 -0700

    Add algorithm option to DigestStrategy.

--- test/strategies/digest-test.js
+++ test/strategies/digest-test.js
@@ -62,7 +62,7 @@ vows.describe('DigestStrategy').addBatch({
   
   'strategy handling a valid request with qop set to "auth"': {
     topic: function() {
-      var strategy = new DigestStrategy(
+      var strategy = new DigestStrategy({ qop: 'auth' },
         function(username, done) {
           done(null, 'secret');
         },
@@ -87,6 +87,47 @@ vows.describe('DigestStrategy').addBatch({
         req.method = 'HEAD';
         req.headers = {};
         req.headers.authorization = 'Digest username="bob", realm="Users", nonce="T1vogipt8GzzWyCZt7U3TNV5XsarMW8y", uri="/", cnonce="MTMxOTkx", nc=00000001, qop="auth", response="7495a912e5c52e1e9ac92793c6f5c229"';
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should not generate an error' : function(err, user) {
+        assert.isNull(err);
+      },
+      'should authenticate' : function(err, user) {
+        assert.equal(user.username, 'bob');
+      },
+    },
+  },
+  
+  'strategy handling a valid request with qop set to "auth" and algorithm set to "MD5-sess"': {
+    topic: function() {
+      var strategy = new DigestStrategy({ qop: 'auth', algorithm: 'MD5-sess' },
+        function(username, done) {
+          done(null, 'secret');
+        },
+        function(username, options, done) {
+          done(null, { username: username });
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(null, user);
+        }
+        strategy.fail = function() {
+          self.callback(new Error('should not be called'));
+        }
+        
+        req.method = 'HEAD';
+        req.headers = {};
+        req.headers.authorization = 'Digest username="bob", realm="Users", nonce="Ag1nqGybX7GXpXGWjTJs0pCCRboeLnbI", uri="/", cnonce="MTMxOTkx", nc=00000001, qop="auth", response="db5b2989137bf89622d8cb1ea583eec9", algorithm="MD5-sess"';
         process.nextTick(function () {
           strategy.authenticate(req);
         });
@@ -288,6 +329,44 @@ vows.describe('DigestStrategy').addBatch({
         // fail action was called, resulting in test callback
         assert.isNull(err);
         assert.match(challenge, /^Digest realm="Users", nonce="\w{32}", opaque="abcdefg1234"$/);
+      },
+    },
+  },
+  
+  'strategy handling a request without authorization credentials with algorithm option set': {
+    topic: function() {
+      var strategy = new DigestStrategy({ algorithm: 'MD5-sess' },
+        function(username, done) {
+          done(null, 'secret');
+        },
+        function(username, options, done) {
+          done(null, { username: username });
+        }
+      );
+      return strategy;
+    },
+    
+    'after augmenting with actions': {
+      topic: function(strategy) {
+        var self = this;
+        var req = {};
+        strategy.success = function(user) {
+          self.callback(new Error('should not be called'));
+        }
+        strategy.fail = function(challenge) {
+          self.callback(null, challenge);
+        }
+        
+        req.headers = {};
+        process.nextTick(function () {
+          strategy.authenticate(req);
+        });
+      },
+      
+      'should fail authentication with challenge' : function(err, challenge) {
+        // fail action was called, resulting in test callback
+        assert.isNull(err);
+        assert.match(challenge, /^Digest realm="Users", nonce="\w{32}", algorithm=MD5-sess$/);
       },
     },
   },
